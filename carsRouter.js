8f67efb77977a853e5442048c114c0336a841d11
ryanp <ryanpanimation@gmail.com>
2020-07-27 20:19:59 -0500

    stretch put and delete

--- carsRouter.js
+++ carsRouter.js
@@ -20,6 +20,30 @@ router.post("/", validateBody, async (req, res, next) => {
   }
 });
 
+router.put("/:id", validateBody, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = await db("cars")
+      .where("id", id)
+      .update({ ...req.body });
+    res.status(200).json({ ...req.body });
+  } catch (error) {
+    console.log(error);
+    next({ status: 500, errorMessage: "Error when updating this entry" });
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = await db("cars").where("VIN", id).del();
+    res.status(200).json({ message: "Car Record Deleted" });
+  } catch (error) {
+    console.log(error);
+    next({ status: 500, errorMessage: "Error when deleting this entry" });
+  }
+});
+
 async function validateBody(req, res, next) {
   const { VIN, make, model, mileage } = req.body;
   if (VIN && make && model && mileage) {
